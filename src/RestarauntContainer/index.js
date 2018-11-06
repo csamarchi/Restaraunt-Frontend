e5d30324773855e8d2d7512eec9fd231fcd08cff
Christine Samarchi <christinesamarchi@Christines-MacBook-Air.local>
2018-11-05 18:26:35 -0600

    rendered restaraunt container

---
+++ src/RestarauntContainer/index.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+class RestarauntContainer extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      restaraunt: []
+    }
+  }
+
+  render() {
+    return(
+      <div>
+        <h1> restaraunt container </h1>
+      </div>
+    )
+  }
+}
+
+export default RestarauntContainer;
