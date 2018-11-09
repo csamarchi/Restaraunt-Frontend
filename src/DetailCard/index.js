56d9621dd773e0d2698ae743e8a857dc3355f6df
Christine Samarchi <csamarchi@gmail.com>
2018-11-09 13:55:26 -0600

    pull down

--- src/DetailCard/index.js
+++ src/DetailCard/index.js
@@ -5,7 +5,7 @@ import { Link }from 'react-router-dom';
 
 const DetailCard = (props) => (
   <Modal open={props.showModal} closeIcon onClick={props.closeModal}>
-    <Modal.Header>Select a Photo</Modal.Header>
+    <Modal.Header>{props.restaurant.name}</Modal.Header>
       <Modal.Content image>
         <Image wrapped size='medium' src= {props.restaurant.featured_image}/>
         <Modal.Description>
